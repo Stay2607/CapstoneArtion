23c38d97b6b391caa15bb73200a77ddc270384b6
Hakim <hakim.vengeance155@gmail.com>
2022-12-05 14:35:09 +0700

    Update profile & Pembuatan setting page

--- app/src/main/java/com/dicoding/picodiploma/capstoneartion/main/ProfileFragment.kt
+++ app/src/main/java/com/dicoding/picodiploma/capstoneartion/main/ProfileFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.dicoding.picodiploma.capstoneartion.databinding.FragmentProfileBinding
 import com.dicoding.picodiploma.capstoneartion.login.LoginActivity
+import com.dicoding.picodiploma.capstoneartion.setting.SettingActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -29,26 +30,18 @@ class ProfileFragment : Fragment() {
 
     private lateinit var auth: FirebaseAuth
 
-    private var param1: String? = null
-    private var param2: String? = null
-
-    /*override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-
-        auth = Firebase.auth
-        onViewCreate()
-
-    }*/
-
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         auth = Firebase.auth
         btnLogout()
+        btnSetting()
+    }
+
+    private fun btnSetting() {
+        binding?.btnSetting?.setOnClickListener {
+            val intent = Intent(activity, SettingActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     private fun btnLogout() {
@@ -64,31 +57,12 @@ class ProfileFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         return _binding!!.root
         //return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 
 }
